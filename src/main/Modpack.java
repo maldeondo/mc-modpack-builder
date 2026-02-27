6f8e82eb799498cff153e515e927a7a2b8003636
Mario Aldeondo <ledge.locust3106@eagereverest.com>
2026-02-27 22:29:37 +0100

    finish first implementation of dynamic-style tables

--- src/main/Modpack.java
+++ src/main/Modpack.java
@@ -1,17 +1,14 @@
 public class Modpack {
     private final int MAX_MODS;
-    private Mod[] list; // mod array
+    private Mod[] modlist; // mod array
 
-    private int max_format_chars; // max chars among all names
+    private int[] longest_chars = {Utils.MINIMUM_NAME_LENGHT, Utils.MINIMUM_VERSION_LENGHT};
     
-    private int current_mods;
+    private int current_mods = 0;
 
     public Modpack(int max_mods) {
         this.MAX_MODS = max_mods;
-        list = new Mod[MAX_MODS];
-
-        max_format_chars = 0;
-        current_mods = 0;
+        modlist = new Mod[MAX_MODS];
     }
 
     public Modpack() { this(64); }
@@ -20,7 +17,7 @@ public class Modpack {
 
     public Mod getMod(int index) throws IndexOutOfBoundsException {
         if (!validIndex(index)) throw new IndexOutOfBoundsException();
-        else return list[index];
+        else return modlist[index];
     }
 
     public int getModNum() { return current_mods + 1; }
@@ -36,13 +33,14 @@ public class Modpack {
         else {
             
             for (int i = current_mods - 1; i > index; i--) {
-                list[i] = list[i - 1]; 
+                modlist[i] = modlist[i - 1]; 
             }
 
-            list[index] = mod;
+            modlist[index] = mod;
             current_mods++;
 
-            updateChars(mod.getName());
+            updateLongest(mod.getName(), Utils.LONGEST_NAME_INDEX, 3);
+            updateLongest(mod.getVersion(), Utils.LONGEST_VERSION_INDEX, 7);
         }
     }
 
@@ -53,32 +51,33 @@ public class Modpack {
         else {
 
             for (int i = index; i < current_mods - 1; i++) {
-                list[index] = list[index + 1];
+                modlist[index] = modlist[index + 1];
             }
 
             current_mods--;
-            updateAllChars();
+            updateLongestFull();
         }
     }
 
-    private void updateAllChars() {
-        max_format_chars = 0;
+    private void updateLongestFull() {
+        longest_chars[Utils.LONGEST_NAME_INDEX] = 3;
+        longest_chars[Utils.LONGEST_VERSION_INDEX] = 7;
 
         for (int i = 0; i < current_mods; i++) {
-            updateChars(list[i].getName());
+            updateLongest(modlist[i].getName(), Utils.LONGEST_NAME_INDEX, 3);
+            updateLongest(modlist[i].getVersion(), Utils.LONGEST_VERSION_INDEX, 7);
         }
     }
+    private void updateLongest(String data, int field, int minimum) {
+        int chars = data.length();
 
-    private void updateChars(String name) {
-        int chars = name.length();
-
-        if (chars > max_format_chars) max_format_chars = chars;
+        if (chars > minimum && chars > longest_chars[field]) longest_chars[field] = chars;
     }
 
-
     public void printModpack() throws Exception {
-        String dynamic_table_format = Utils.TABLE_FORMAT_1 + String.format("%d", max_format_chars) + Utils.TABLE_FORMAT_2;
-        String dynamic_table_separator = Utils.tableSeparator(max_format_chars);
+        String dynamic_table_format = Utils.tableFormat(longest_chars);
+        
+        String dynamic_table_separator = Utils.tableSeparator(longest_chars);
 
         Mod temp;
         StringBuilder block = new StringBuilder();
@@ -89,7 +88,6 @@ public class Modpack {
 
         for (int i = 0; i < current_mods; i++) {
             temp = getMod(i);
-            System.out.println(Utils.clientStatusFormat(temp.getModStatus()));
             block.append(String.format(dynamic_table_format, String.valueOf(i + 1),  temp.getName(), temp.getVersion(), Utils.clientTypeFormat(temp.getModType()), Utils.clientStatusFormat(temp.getModStatus())));
         }
 
--- src/main/Utils.java
+++ src/main/Utils.java
@@ -19,7 +19,7 @@ public class Utils {
 
     public static String clientStatusFormat(int status) throws Exception {
         return switch (status) {
-            case 0 -> "";
+            case 0 -> "-";
             case 1 -> "A";
             case 2 -> "A+P";
             case 3 -> "C";
@@ -31,14 +31,23 @@ public class Utils {
         return new String(new char[amount]).replace("\0", text);
     }
 
+    public static final int LONGEST_NAME_INDEX = 0;
+    public static final int LONGEST_VERSION_INDEX = 1;
+
+    public static final int MINIMUM_NAME_LENGHT = 3;
+    public static final int MINIMUM_VERSION_LENGHT = 7;
+
     // Index -- Name -- Version -- Type -- Status
     public static final String TABLE_FORMAT = "%-3s| %-15s | %-10s | %-4s | %-6s |\n";
-    public static final String TABLE_FORMAT_1 = "%-3s| %-";
-    public static final String TABLE_FORMAT_2 = "s | %-10s | %-4s | %-6s |\n";
+
+    public static String tableFormat(int[] longest_chars) {
+        return "%-3s| %-" + longest_chars[LONGEST_NAME_INDEX] + "s | %-" + longest_chars[LONGEST_VERSION_INDEX] + "s | %-4s | %-6s |\n";
+    }
 
     public static final String TABLE_SEPARATOR = "---|-----------------|------------|------|--------|\n";
 
-    public static final String tableSeparator(int max_name) {
-        return "---|" + repeat(max_name + 2, "-") + "|------------|------|--------|\n";
+    public static final String tableSeparator(int[] longest_chars) {
+        return "---|" + repeat(longest_chars[LONGEST_NAME_INDEX] + 2, "-") + "|" + repeat(longest_chars[LONGEST_VERSION_INDEX] + 2, "-") + "|------|--------|\n";
     }
+
 }
