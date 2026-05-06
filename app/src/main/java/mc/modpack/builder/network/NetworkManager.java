fb4ec50417787ba8c34b243d52b4c7d066edd3f8
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-05-06 13:04:38 +0200

    Added function that retrieves the name using the mod id

--- app/src/main/java/mc/modpack/builder/network/NetworkManager.java
+++ app/src/main/java/mc/modpack/builder/network/NetworkManager.java
@@ -1,10 +1,21 @@
 package mc.modpack.builder.network;
 
+import java.io.IOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class NetworkManager {
+    private final String key;
+
+    public NetworkManager(String key) {
+        this.key = key;
+    }
+
     //Gets the mod name from the curseforge ID
-    public String getModName(String uid) {
+    public String getModName(String uid) throws IOException, InterruptedException {
+        PetitionResult result = PetitionMaker.makePetition("v1/mods/" + uid, key);
 
-
-        return "";
+        JsonObject obj = JsonParser.parseString(result.getBody()).getAsJsonObject();
+        return obj.get("data").getAsJsonObject().get("name").getAsString();
     }
 }
