d1e32e3cdbb9d338cfd5fd4282db23e5dd1388dc
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-05-06 13:03:07 +0200

    Wraps an static function that is in charge of making the petition and handling petition errors that may occur

--- app/src/main/java/mc/modpack/builder/network/PetitionMaker.java
+++ app/src/main/java/mc/modpack/builder/network/PetitionMaker.java
@@ -1,13 +1,15 @@
 package mc.modpack.builder.network;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class PetitionMaker {
     public static String BASE_URL = "https://api.curseforge.com/";
 
-    public void makePetition(String url, String key) {
+    public static PetitionResult makePetition(String url, String key) throws IOException, InterruptedException {
         //Creating the client that will make the petition
         HttpClient client = HttpClient.newHttpClient();
 
@@ -18,5 +20,11 @@ public class PetitionMaker {
                 .header("x-api-key", key)
                 .GET()
                 .build();
+
+        //Making the petition
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //Building the result
+        return new PetitionResult(response.statusCode(), response.body(), response.headers());
     }
 }
