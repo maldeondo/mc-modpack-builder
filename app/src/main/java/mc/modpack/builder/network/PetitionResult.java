a0a03c8e033905527042c147d5452002fde13320
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-05-08 13:43:55 +0200

    Fixed petitionmaker to directly return The body as a JSONObject,

--- app/src/main/java/mc/modpack/builder/network/PetitionResult.java
+++ app/src/main/java/mc/modpack/builder/network/PetitionResult.java
@@ -1,23 +1,27 @@
 package mc.modpack.builder.network;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.net.http.HttpHeaders;
 
 public class PetitionResult {
     private int status;
-    private String body;
+    private JsonObject body;
     private HttpHeaders headers;
 
     public PetitionResult(int status, String body, HttpHeaders headers) {
         this.status = status;
-        this.body = body;
         this.headers = headers;
+
+        this.body = JsonParser.parseString(body).getAsJsonObject();
     }
 
     public int getStatus() {
         return status;
     }
 
-    public String getBody() {
+    public JsonObject getBody() {
         return body;
     }
 
